5cb04759083f2843f8b2d6ab5aed56ae8841795a
Alexgm17 <agallegomorilla@gmail.com>
2021-05-07 13:46:15 +0200

    Collider de Enemigos Corregio y Menú de Pausa Mejorado

--- Assets/Scripts/DamageObject.cs
+++ Assets/Scripts/DamageObject.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class DamageObject : MonoBehaviour
 {
-  
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Player"))
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,15 +7,31 @@ public class UIManager : MonoBehaviour
 {
     public GameObject optionsPanel;
 
+    public bool optionsPanelActive = false;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("escape") && optionsPanelActive == false)
+        {
+            OptionsPanel();
+        }
+        else if (Input.GetKeyDown("escape") && optionsPanelActive == true)
+        {
+            Return();
+        }
+    }
+
     public void OptionsPanel()
     {
         Time.timeScale = 0;
+        optionsPanelActive = true;
         optionsPanel.SetActive(true);
     }
 
     public void Return()
     {
         Time.timeScale = 1;
+        optionsPanelActive = false;
         optionsPanel.SetActive(false);
     }
 
@@ -27,6 +43,7 @@ public class UIManager : MonoBehaviour
 
     public void GoMainMenu()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("");
     }
 
