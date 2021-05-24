c0c8b783add011da032bf08f5630bbc71ef79efe
M1NE77 <alfonsommm77@gmail.com>
2021-05-24 15:25:59 +0200

    + (Mundo 2) Nivel 2 Alpha + Beta Plant

---
+++ Assets/Scripts/Enemies/Plant/PlantEnemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantEnemy : MonoBehaviour
+{
+    private float waitedTime;
+
+    public float waitedTimeToAttack = 3;
+
+    public Animator animator;
+
+    public GameObject bulletPrefab;
+
+    public Transform launchSpawnPoint;
+
+    private void Start()
+    {
+        waitedTime = waitedTimeToAttack;
+    }
+
+    private void Update()
+    {
+        if (waitedTime <= 0)
+        {
+            waitedTime = waitedTimeToAttack;
+            animator.Play("PlantAttack");
+            Invoke("LaunchBullet", 0.5f);
+        }
+        else
+        {
+            waitedTime -= Time.deltaTime;
+        }
+    }
+
+    public void LaunchBullet()
+    {
+        GameObject newBullet;
+
+        newBullet = Instantiate(bulletPrefab, launchSpawnPoint.position, launchSpawnPoint.rotation);
+    }
+}
