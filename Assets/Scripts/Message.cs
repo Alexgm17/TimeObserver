258367f46bac53bf5b48a1b1c966195e9c55658f
M1NE77 <alfonsommm77@gmail.com>
2021-05-26 15:17:47 +0200

    + Sistema de mensaje en pantalla agregado y completamente funcional

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message : MonoBehaviour
+{
+    public GameObject image;
+    public GameObject key;
+    public GameObject text;
+    public AudioSource jump;
+    public static bool hasrun;
+    public AudioSource audio;
+
+    void Start()
+    {
+        if (!hasrun)
+        {
+            Time.timeScale = 0;
+            jump.mute = true;
+            image.gameObject.SetActive(true);
+            key.gameObject.SetActive(true);
+            text.gameObject.SetActive(true);
+            hasrun = true;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape") || Input.GetKeyDown("e"))
+        {
+            Time.timeScale = 1;
+            jump.mute = false;
+            image.gameObject.SetActive(false);
+            key.gameObject.SetActive(false);
+            text.gameObject.SetActive(false);
+            gameObject.GetComponent<Message>().enabled = false;
+            audio.Play();
+
+        }
+    }
+}
