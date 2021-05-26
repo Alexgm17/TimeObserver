d72a2a8dedfdd2a98687ae7d9b3084df662622f7
M1NE77 <alfonsommm77@gmail.com>
2021-05-26 14:04:25 +0200

    + (Main Menu) Pantalla de cargando y añadido sonido al clicar

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using TMPro;
 
 public class SceneLoader : MonoBehaviour
 {
+    public TMP_Text text;
+    public GameObject startScreen;
+
     public void LoadGame()
     {
-        SceneManager.LoadScene("GameMenu");
+        startScreen.gameObject.SetActive(false);
+        text.gameObject.SetActive(true);
+        Invoke("ChangeScene", 1f);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("GameMenu");
     }
 }
