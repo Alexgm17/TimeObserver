9ac0314188f958e383be2b2893700150919527a4
M1NE77 <alfonsommm77@gmail.com>
2021-05-26 21:53:28 +0200

    + Efecto background message

--- Assets/Scripts/Message.cs
+++ Assets/Scripts/Message.cs
@@ -11,6 +11,7 @@ public class Message : MonoBehaviour
     public static bool hasrun;
     public AudioSource audio;
     public GameObject optionsPanel;
+    public GameObject messageBackground;
 
     void Start()
     {
@@ -18,10 +19,11 @@ public class Message : MonoBehaviour
         {
             Time.timeScale = 0;
             jump.mute = true;
+            hasrun = true;
+            messageBackground.gameObject.SetActive(true);
             image.gameObject.SetActive(true);
             key.gameObject.SetActive(true);
             text.gameObject.SetActive(true);
-            hasrun = true;
         }
     }
 
@@ -31,6 +33,7 @@ public class Message : MonoBehaviour
         {
             Time.timeScale = 1;
             jump.mute = false;
+            messageBackground.gameObject.SetActive(false);
             image.gameObject.SetActive(false);
             key.gameObject.SetActive(false);
             text.gameObject.SetActive(false);
