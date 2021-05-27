19a66791030ed0d8ae288b46ab9ac5c70e79de22
M1NE77 <alfonsommm77@gmail.com>
2021-05-27 14:17:37 +0200

    + Funcionalidad de resolución añadida y terminada / + Corregida toda la escena de main menu

--- Assets/Scripts/Options/Fullscreen.cs
+++ Assets/Scripts/Options/Fullscreen.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class Fullscreen : MonoBehaviour
 {
     public Toggle toggle;
+    public Dropdown resolutionsDropdown;
+    Resolution[] resolutions;
 
     void Start()
     {
@@ -17,10 +19,43 @@ public class Fullscreen : MonoBehaviour
         {
             toggle.isOn = false;
         }
+
+        CheckResolution();
     }
 
     public void SetFullScreen(bool fullScreen)
     {
         Screen.fullScreen = fullScreen;
+    }
+
+    public void CheckResolution()
+    {
+        resolutions = Screen.resolutions;
+        resolutionsDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int actualResolution = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " | " + resolutions[i].refreshRate+" HZ";
+            options.Add(option);
+
+            if (Screen.fullScreen && resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                actualResolution = i;
+            }
+        }
+
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = actualResolution;
+        resolutionsDropdown.RefreshShownValue();
+        resolutionsDropdown.value = PlayerPrefs.GetInt("ResolutionIndex", 0);
+    }
+
+    public void ChangeResolution(int resolutionIndex)
+    {
+        PlayerPrefs.SetInt("ResolutionIndex", resolutionsDropdown.value);
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
