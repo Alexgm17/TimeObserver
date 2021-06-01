f588a51886c4d720ec92a21d9eca4dd02dd99f9b
Alexgm17 <agallegomorilla@gmail.com>
2021-06-02 00:28:14 +0200

    + Funcionalidad Cambiar Skins Agregada (Falta por Incorporar al Game Menu)

--- Assets/Scripts/PlayerSelect.cs
+++ Assets/Scripts/PlayerSelect.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public enum Player {Scientist, MaskDude, Frog, PinkMan, VirtualGuay};
+    public Player playerSelected;
+
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    public RuntimeAnimatorController[] playersController;
+    public Sprite[] playersRenderer;
+
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        switch (playerSelected)
+        {
+            case Player.Scientist:
+                spriteRenderer.sprite = playersRenderer[0];
+                animator.runtimeAnimatorController = playersController[0];
+                break;
+            case Player.MaskDude:
+                spriteRenderer.sprite = playersRenderer[1];
+                animator.runtimeAnimatorController = playersController[1];
+                break;
+            case Player.Frog:
+                spriteRenderer.sprite = playersRenderer[2];
+                animator.runtimeAnimatorController = playersController[2];
+                break;
+            case Player.PinkMan:
+                spriteRenderer.sprite = playersRenderer[3];
+                animator.runtimeAnimatorController = playersController[3];
+                break;
+            case Player.VirtualGuay:
+                spriteRenderer.sprite = playersRenderer[4];
+                animator.runtimeAnimatorController = playersController[4];
+                break;
+            default:
+                break;
+        }
     }
 }
